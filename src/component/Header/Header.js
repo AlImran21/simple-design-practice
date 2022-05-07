105f3c5b53c20bb539768220141fa7c49773282f
Al-Imran <alimran21feb@gmail.com>
2022-05-08 03:46:29 +0600

    added header and popup

---
+++ src/component/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './Header.Module.css';
+import Logo from '../../images/logo.png';
+import UserImage from '../../images/user.png';
+import { Link } from 'react-router-dom';
+
+
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <nav>
+                <img src={Logo} alt="" />
+                <ul>
+                    <li><Link to='/home'>Home</Link></li>
+                    <li><Link to='/popUp'>PopUp</Link></li>
+                    <li><Link to='/women'>Women</Link></li>
+                    <li><Link to='/kids'>Kids</Link></li>
+                    <li><Link to='/living'>Home & Living</Link></li>
+                </ul>
+            </nav>
+
+            <div className='text-box'>
+                <h1>Great Discount On Latest Collection</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Cumque quod quia, iure tempora nobis magni aperiam <br /> provident maxime nisi vitae.</p>
+                <button className='shop-now-button'>Shop Now</button>
+            </div>
+
+            <div className="user-box">
+                <img src={UserImage} alt="" />
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Header;
---
+++ src/component/Header/PopUp/PopUp.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PopUpLogo from '../../../images/404-tick.png';
+import './PopUp.Module.css';
+
+
+const PopUp = () => {
+
+    let popup = document.getElementById('popup');
+    const handleSubmitButton = () => {
+        popup?.classList?.add('open-popup');
+    }
+
+    const handleOkButton = () => {
+        popup?.classList?.remove('open-popup');
+    }
+
+
+    return (
+        <div className='container'>
+            <button type='submit' className='submit-btn' onClick={handleSubmitButton}>Submit</button>
+            <div className="pop-up" id='popup'>
+                <img src={PopUpLogo} alt="" />
+                <h2>Thank You!</h2>
+                <p>Your details has been successfully submitted. Thanks!</p>
+                <button type='button' onClick={handleOkButton}>Ok</button>
+            </div>
+        </div>
+    );
+};
+
+export default PopUp;
